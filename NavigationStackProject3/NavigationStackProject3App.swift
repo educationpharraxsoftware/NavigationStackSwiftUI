80815fdcd6e8c2adc5581dbed952e8103701de70
macstudio <macos@example.com>
2024-05-25 07:57:08 +0300

    Initial Commit

---
+++ NavigationStackProject3/NavigationStackProject3App.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStackProject3App.swift
+//  NavigationStackProject3
+//
+//  Created by YILMAZ ER on 25.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStackProject3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
